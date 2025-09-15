9668c05e0606ad6fb4ea498939373841bb9e762a
igh_life <164069169+17Midas@users.noreply.github.com>
2025-09-15 18:04:19 +0300

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,1 +1,11 @@
-
+for (i = 0; i<=3; i++)
+if (i===0) {
+    console.log('C is fun');
+    }
+else if(i===1){
+    console.log('Python is cool')
+}
+else if (i===2){
+    console.log('JavaScript is amazing')
+}
+{'break'};
